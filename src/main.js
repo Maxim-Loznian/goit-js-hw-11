f622a6a7d8b2efeb3ec9ae6a1260271ab3d3bdec
Maxim <loznyan84@gmail.com>
2024-06-11 10:45:24 +0300

    домашка

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { fetchImages } from './pixabay-api';
+import { clearGallery, renderImages } from './render-functions';
+
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-form input');
+const gallery = document.querySelector('.gallery');
+
+searchForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const query = searchInput.value.trim();
+  if (query === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Search query cannot be empty'
+    });
+    return;
+  }
+
+  clearGallery();
+
+  try {
+    const data = await fetchImages(query);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Sorry, there are no images matching your search query. Please try again.'
+      });
+      return;
+    }
+
+    renderImages(data.hits);
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: error.message
+    });
+  }
+});
